a3cd41c2de9479bf1f609e33502104a894474a08
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2022-11-02 10:57:19 +0700

    a

--- assets/leaderboard.9ede5d84.js
+++
@@ -1,1 +0,0 @@
-import{H as a,g as r}from"./index.d7b7f93e.js";import{o,c as s,f as e}from"./vendor.e9bd5562.js";const _={setup(t){return(c,n)=>(o(),s("main",null,[e(a),e(r)]))}};export{_ as default};
